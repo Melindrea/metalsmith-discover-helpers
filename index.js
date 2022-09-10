08a4b6c646a504d26f01973e15cbe92c3e5ca34e
Marie Hogebrandt <iam@mariehogebrandt.se>
2022-09-10 20:01:18 +0200

    Updating dependencies

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-'use strict'
+'use strict';
 
-var defaults = require('defaults')
-var Handlebars = require('handlebars')
-var walk = require('fs-tools').walk
-var path = require('path')
-
-var onFile = function (file, stats, next) {
-  var fn = null
-  try {
-    fn = require(path.resolve(file))
-  } catch (err) {
-    return next(err)
-  }
-  var id = path.basename(file, path.extname(file))
-  Handlebars.registerHelper(id, fn)
-  next()
-}
+const defaults = require('defaults'),
+  Handlebars = require('handlebars'),
+  fs = require('fs'),
+  path = require('path'),
+  glob = require('glob');
 
 module.exports = function (options) {
   options = defaults(options, {
     directory: 'helpers',
-    pattern: /\.js$/
-  })
+    pattern: '.js'
+  });
   return function (files, metalsmith, done) {
-    walk(metalsmith.path(options.directory), options.pattern, onFile, done)
+    let basedir = metalsmith.path(options.directory) + '/';
+    let fullPath = basedir + '**/*' + options.pattern;
+    glob(fullPath, function (err, files) {
+      if (err) {
+        return done(err)
+      }
+
+      files.forEach(file => {
+        let fn = null;
+        try {
+          fn = require(path.resolve(file))
+        } catch (err) {
+          return done(err)
+        }
+        let id = file.replace(basedir, '').replace(path.extname(file), '').replaceAll('/', '-');
+        Handlebars.registerHelper(id, fn);
+        done();
+      });
+    });
   }
 }
